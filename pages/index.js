f34a54b7434d52aaa9d24ee83825dbba2a62e9ea
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-04 12:30:05 -0300

    Instalando o next-auth e implementando o login através do OAuth (google)

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,24 @@
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Home() {
+  const { data: session } = useSession();
+
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button
+             className="bg-white p-2 px-4 rounded-lg"
+             onClick={() => signIn('google')}
+          >
+            Login with Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
   return (
-   <div className='bg-red-500'>
-      Starting
-   </div>
+    <div>Logged in {session.user.email}</div>
   )
 }
