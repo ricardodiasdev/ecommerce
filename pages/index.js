b4d567293f39885d9d0c082099759f93fa9bc9f0
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-04 22:03:49 -0300

    Modificando index

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,9 @@
-import Nav from "@/components/Nav";
-import { useSession, signIn, signOut } from "next-auth/react";
+import Layout from "@/components/Layout";
 
 export default function Home() {
-  const { data: session } = useSession();
-
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button
-            className="bg-white p-2 px-4 rounded-lg"
-            onClick={() => signIn("google")}
-          >
-            Login with Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className="bg-blue-900 min-h-screen">
-      <Nav />
-      <div>Logged in {session.user.email}</div>
-    </div>
+    <Layout>
+      Dashboard
+    </Layout>
   );
 }
