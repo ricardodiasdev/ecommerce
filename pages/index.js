9a8a8a815cf08df33f117ef13501ed76fa095b41
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-05 11:30:33 -0300

    Implementando nome, email e imagem na dashboard

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,25 @@
 import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 export default function Home() {
+  const { data: session } = useSession();
   return (
     <Layout>
-      Dashboard
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex bg-gray-300 gap-1 text-black rounded-lg overflow-hidden">
+          <Image
+            src={session?.user?.image}
+            alt="Image"
+            width={25}
+            height={25}
+          />
+          <span className="px-2">{session?.user?.name}</span>
+        </div>
+      </div>
     </Layout>
   );
 }
