4bfba432bfebda66db6c3085073c5a64770a897b
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-06 20:59:32 -0300

    Incluindo formulário no New e fazendo a ligação com o BD

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,19 +1,54 @@
 import Layout from "@/components/Layout";
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { toast } from "react-toastify";
+import React, { useState } from "react";
 
 const NewProduct = () => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const route = useRouter();
+
+  const createProduct = async (ev) => {
+    ev.preventDefault();
+    const data = { title, description, price };
+    await axios.post("/api/products", data).then(() => {
+      toast.success("Product created!");
+      route.push("/products");
+    });
+  };
+
   return (
     <Layout>
-      <div className="flex flex-col">
-        <h1>New Product</h1>
-        <label>Product name</label>
-        <input type="text" placeholder="Product name" />
-        <label>Product name</label>
-        <textarea placeholder="Description"></textarea>
-        <label>Product name</label>
-        <input type="text" placeholder="Price" />
-      </div>
-      <button className="btn-primary">Save</button>
+      <form onSubmit={createProduct}>
+        <div className="flex flex-col">
+          <h1>New Product</h1>
+          <label>Product name</label>
+          <input
+            type="text"
+            placeholder="Product name"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Description</label>
+          <textarea
+            placeholder="Description"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+          ></textarea>
+          <label>Price (in USD)</label>
+          <input
+            type="text"
+            placeholder="Price"
+            value={price}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn-primary">
+          Save
+        </button>
+      </form>
     </Layout>
   );
 };
