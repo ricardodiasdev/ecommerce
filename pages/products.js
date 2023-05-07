53c2dbd27a2ade5c2968140fbf6b594d27e115fc
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-06 21:00:53 -0300

    Mostrando os produtos na página products

--- pages/products.js
+++ pages/products.js
@@ -1,16 +1,38 @@
 import Layout from "@/components/Layout";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/products").then((response) => {
+      setProducts(response.data);
+    });
+  }, []);
+
   return (
     <Layout>
-      <Link 
-        href={'/products/new'}
-        className="btn-primary"
-      >
+      <Link href={"/products/new"} className="btn-primary">
         Add new product
       </Link>
+      <table className="basic">
+        <thead>
+          <tr>
+            <td>Product name</td>
+            <td></td>
+          </tr>
+          <tbody>
+            {products.map((product) => (
+              <tr key={product.id}>
+                <td>{product.title}</td>
+                <td>buttons</td>
+              </tr>
+            ))}
+          </tbody>
+        </thead>
+      </table>
     </Layout>
   );
 };
