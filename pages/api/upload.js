0bf205a7588a16cb9577924166890d2fc730567f
Ricardo Dias <ricardodias.dev@gmail.com>
2023-05-08 19:01:29 -0300

    Modificando a api upload

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -2,9 +2,15 @@ import multiparty from "multiparty";
 
 export default async function handle(req, res) {
   const form = new multiparty.Form();
-  form.parse(req, async(err, fields,files) =>{
-
-  })
+  const { fields, files } = await new Promise((resolve, reject) => {
+    form.parse(req, async (err, fields, files) => {
+      if (err) reject(err);
+      resolve({ fields, files });
+    });
+  });
+  console.log("length: ", files);
+  console.log(fields);
+  return res.json("ok");
 }
 
 export const config = {
